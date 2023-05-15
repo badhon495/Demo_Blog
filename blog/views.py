a58fcb50ab74c11a6815431e0b7c9f7f6deb6053
badhon <badhon495@gmail.com>
2023-05-15 23:35:57 +0600

    upto 12:36

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse #we dont need this as we are now using html directly
+
+posts = [
+    {
+        'author' : 'badhon',
+        'title' : 'Blog Post',
+        'Content' : 'First Post Content',
+        'Date posted' : 'August 27, 2018'
+    },
+    
+    {
+        'author' : 'badhon_xtra',
+        'title' : 'New Blog Post',
+        'Content' : 'Second Post Content',
+        'Date posted' : 'August 27, 2019'
+    }
+]
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    # return HttpResponse('<h1>Blog Home</h1>') #eta use kora jabe na karon httpsrespons e eto boro html code lekha jhamela. tai amra arekta use kori
+    return render(request, 'blog/home.html')
 # Create your views here.
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html') 
