98c6df089e24f104eafff38e7f63be6ce79bde25
badhon495 <badhon495@gmail.com>
2023-05-29 12:32:11 +0600

    commit on 10th video

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
+# from .views import PostListView
 from . import views  # dot diye same dirctory theke views file ta import kortesi
 
 
 urlpatterns = [
-    path('', views.home, name = 'blog-home'),
+    path('', views.PostListView.as_view(), name = 'blog-home'),
     #first parameter empty karon home page ke indicate kortese.
     #second parameter e views.home ke deoa hoyeche. kron eita oikahne niye jabae. httpsresponse return kore
     #third parameter hocche name for the path
     
-    path('about/', views.about, name = 'blog-about')
+    path('about/', views.about, name = 'blog-about'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name = 'post-detail'), #pk  means primary key. post/<int:pk> means it will got to post/primary key. here primary key post id.
+    path('post/new/', views.PostCreateView.as_view(), name = 'post-create'), #nameofthemodel_form for this it will be post_form
+    path()
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 # from django.http import HttpResponse #we dont need this as we are now using html directly
 from .models import Post
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 def home(request):
@@ -9,7 +11,32 @@ def home(request):
         'posts' : Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
-# Create your views here.
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted'] #we have put a minus before it because it will sort the post based on new to old time.
+
+class PostDetailView(DetailView): #<app>/<model>_<viewpoint>.html --> ei jaigai khujbe era here app name is blog and model name is Post. and viewpoint would be detail
+    model = Post
+     
+class PostCreateView(LoginRequiredMixin,CreateView): # we need LoginRequireMixin to ensure that we are logged in when we creating a new post. otherwise it will send us to log in page.
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self,form):
+        form.instance.author = self.request.user #making the post author to current logged in user
+        return super().form_valid(form)  #validating the form and creating the post
+
+class PostUpdateView(LoginRequiredMixin,CreateView): # we need LoginRequireMixin to ensure that we are logged in when we creating a new post. otherwise it will send us to log in page.
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self,form):
+        form.instance.author = self.request.user #making the post author to current logged in user
+        return super().form_valid(form)  #validating the form and creating the post
 
 
 def about(request):
